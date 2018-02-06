8edd4da2f0b55c5870ed2528dbc2b3f8f289af76
ManjiriBirajdar <manjiri_birajdar@outlook.com>
2018-02-06 16:01:27 +0100

    own implementation for writing dead writes in file

--- experiments/dynamic_dead_write_analysis.js
+++ experiments/dynamic_dead_write_analysis.js
@@ -8,17 +8,10 @@
 
 (function (sandbox) {      
     
-        function MyAnalysis() {
-          
-            var traceWriter = new sandbox.TraceWriter("experiments/trace.log")
-            var stack = [];  
-             var stringList = [];
-
-            function logEvent(str) {                
-              
-                traceWriter.logToFile(str+"\n");
-                //@todo dump and clear the logs array once its size exceeds some constant, say 1024
-            }
+        function MyAnalysis() {          
+		
+            var stack = [];               
+           
 
             this.read = function (iid, name, val, isGlobal, isScriptLocal) {
                 var frameId = sandbox.smemory.getIDFromShadowObjectOrFrame(sandbox.smemory.getShadowFrame(name));
@@ -45,22 +38,18 @@
             };
     
             this.endExecution = function () {
-    
+				
+				const fs = require('fs');
+				var traceWfh = fs.openSync('experiments/dead_writes_location.txt', 'w');
+				
+				
                 console.log("------------------------Total Dead Writes in Program (including variables initialized but not used)-------------------------");
-
+			
                 for(var i in stack)
                 {
-                    console.log("Variable name is " +stack[i]);
-                    logEvent(stack[i]);
-                }
-
-                traceWriter.stopTracing();
-                var tw = new sandbox.TraceWriter("strings.json");
-                tw.logToFile(JSON.stringify(stringList)+"\n");
-                tw.stopTracing();
-                tw = new sandbox.TraceWriter("smap.json");
-                tw.logToFile(JSON.stringify(sandbox.smap)+"\n");
-                tw.stopTracing();		
+                    console.log("Variable name is " +stack[i]);     
+					fs.writeSync(traceWfh, stack[i] + "\n");
+                }							
             };
         }    
         sandbox.analysis = new MyAnalysis();    
@@ -70,6 +59,6 @@
 
 COMMAND TO RUN:
 
-node src/js/commands/jalangi.js --inlineIID --inlineSource --analysis src/js/sample_analyses/ChainedAnalyses.js --analysis src/js/runtime/SMemory.js --analysis experiments\TraceWriter.js --analysis experiments\dynamic_dead_write_analysis.js experiments\dead_write_prg_1.js
+node src/js/commands/jalangi.js --inlineIID --inlineSource --analysis src/js/sample_analyses/ChainedAnalyses.js --analysis src/js/runtime/SMemory.js --analysis experiments\dynamic_dead_write_analysis.js experiments\dead_write_prg_1.js
 
 */
